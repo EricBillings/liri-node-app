3a58c2278541e49ee772c395a0150263921207ec
Eric Billings <billings.company@gmail.com>
2019-08-14 08:17:50 -0400

    added spotify functionality

--- liri.js
+++ liri.js
@@ -1,12 +1,31 @@
+
 require("dotenv").config();
-var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
+const keys = require("./keys.js");
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
 
 
 const commandType = process.argv[2];
 
-const userSelection = process.argv[3];
+const userSelection = process.argv.slice(3).join(" ");
+
+console.log(userSelection);
 
 
-console.log(commandType + " " + userSelection);
+if (commandType === "spotify-this-song") {
+    spotify
+        .search({ type: 'track', query: userSelection, limit: 1  })
+        .then(function (response) {
+            let musicInfo = {
+                artist: response.tracks.items[0].album.artists[0].name,
+                song: response.tracks.items[0].name,
+                link: response.tracks.items[0].external_urls.spotify,
+                album: response.tracks.items[0].album.name
+            };
+
+                console.log(JSON.stringify(musicInfo, "", 2));
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
